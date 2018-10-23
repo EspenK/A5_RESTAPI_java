50800b812a7392ba4db207f6b8ed9840f1f378a3
espen <espenk95@gmail.com>
2018-10-23 22:39:40 +0200

    Add makeUrl() method that will create the URL we will connect to.

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -9,6 +9,40 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 
+/**
+ * Request is used to send GET and POST requests.
+ */
 public class Request {
-
+    /**
+     * Create and return a URL object.
+     *
+     * @param baseUrl The base url (website).
+     * @param path The API endpoint.
+     * @param params Key value pairs for parameters.
+     * @return URL object.
+     */
+    private URL makeUrl(String baseUrl, String path, JSONObject params) {
+        URL url = null;
+        String urlString = baseUrl + path;
+        if (params != null) {
+            urlString += "?";
+            StringBuilder stringBuilder = new StringBuilder();
+            Iterator<String> keys = params.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                if (stringBuilder.length() > 0) {
+                    stringBuilder.append("&");
+                }
+                stringBuilder.append(key).append("=").append(params.getString(key));
+            }
+            urlString += stringBuilder.toString();
+        }
+        System.out.println(urlString);
+        try {
+            url = new URL(urlString);
+        } catch (IOException e) {
+            System.out.println("Something went wrong with the URL: " + e.getMessage());
+        }
+        return url;
+    }
 }
