3562f3932d493ae22016fa55f2ac2d8ce4646f12
espen <espenk95@gmail.com>
2018-10-23 22:33:59 +0200

    Add Request class.

---
+++ src/main/java/Request.java
@@ -0,0 +1,14 @@
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+public class Request {
+
+}
